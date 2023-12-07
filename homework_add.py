34e971f67e49f84f484d6734c606528bb14578a2
Dumerat <hermelineyoan@gmail.com>
2023-12-07 19:41:34 +0100

    choice list done but it doesn't work

--- homework_add.py
+++ homework_add.py
@@ -5,37 +5,40 @@ class Homework_add(Extension):
         super().__init__()
         self.bot = bot
         self.db = db
-
-    def get_sub(self):
+        
         subject = self.db["homework"].find().sort("subject")
         subject_list = set(",".join(sub["subject"] for sub in subject).split(",")) # create a set of unique subject in all homework
-        return [SlashCommandChoice(name=i, value=i) for i in subject_list]
+        self.choice_list =[SlashCommandChoice(name=i, value=i) for i in subject_list]
+        self.setup_commands()
 
-    @slash_command(
-        name="add",
-        description="Permet d'ajouter un devoir pour tout le monde")
-    @slash_option(
-        name = "name",
-        description= "nom du devoir",
-        required=True,
-        opt_type=OptionType.STRING,
-        )
-    @slash_option(
-        name = "matière",
-        description= "nom de la matière",
-        required=True,
-        opt_type=OptionType.STRING,
-        choices=get_sub
-        )
-    @slash_option(
-        name = "named",
-        description= "nom du devoir",
-        required=True,
-        opt_type=OptionType.INTEGER,
-        choices=[
-            SlashCommandChoice(name="poulet", value=1),
-            SlashCommandChoice(name="nuggets", value=2)
-        ]
-        )
-    async def message_add(self, ctx):
-        await ctx.send(f"yo ça marche ")
+    def setup_commands(self):
+        @slash_command(
+            name="add",
+            description="Permet d'ajouter un devoir pour tout le monde")
+        @slash_option(
+            name = "name",
+            description= "nom du devoir",
+            required=True,
+            opt_type=OptionType.STRING,
+            )
+        @slash_option(
+            name = "matière",
+            description= "nom de la matière",
+            required=True,
+            opt_type=OptionType.STRING,
+            choices=self.choice_list
+            )
+        @slash_option(
+            name = "named",
+            description= "nom du devoir",
+            required=True,
+            opt_type=OptionType.INTEGER,
+            choices=[
+                SlashCommandChoice(name="poulet", value=1),
+                SlashCommandChoice(name="nuggets", value=2)
+            ]
+            )
+        async def message_add(self, ctx):
+            await ctx.send(f"yo ça marche {ctx}")
+
+        self.bot.add_command(message_add)
